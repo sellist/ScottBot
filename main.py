bb25239ea2e17af0c808bbc99e59ffded6f98b49
Scott Ellis <scottellis94@gmail.com>
2022-03-24 01:55:33 -0400

    Put name functions in own module, created on_events module for future, minor tweaks and changes

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import discord
 import os
-import random
+from name import Names
 
 client = discord.Client()
+names = Names('names.txt')
 
 try:
     TOKEN = str(os.environ['DISCORD_TOKEN'])
-    print('token retrieved')
+    print('Token retrieved')
     ADMIN_ID = int(os.environ['ADMIN_ID'])
-    print('admin_id retrieved')
+    print('Admin_id retrieved')
 except KeyError:
     print("Enter discord token: ")
     os.environ['DISCORD_TOKEN'] = input()
@@ -18,13 +19,11 @@ except KeyError:
 TOKEN = str(os.environ['DISCORD_TOKEN'])
 ADMIN_ID = int(os.environ['ADMIN_ID'])
 
-NAMES_FILE = 'names.txt'
-
 
 # log in event
 @client.event
 async def on_ready():
-    print("Retrieved token and ID")
+    print("Successfully loaded vars from env")
     print(f"Logged in as {client.user}")
 
 
@@ -41,9 +40,7 @@ async def on_message(message):
         is_admin = True
 
     # !test
-    """
-    Checks if user who put !test is an admin, and returns their ID number
-    """
+    # Checks if user who put !test is an admin, and returns their ID number
     if message.content.startswith('!test'):
         if is_admin:
             await message.channel.send(f'I saw an admin say {message.content} from {message.author.id}')
@@ -51,58 +48,30 @@ async def on_message(message):
             await message.channel.send(f'I saw a non admin say {message.content} from {message.author.id}')
 
     # !addname
-    """
-    Takes in name from user input, and updates names.txt with it
-    """
+    # Takes in name from user input, and updates names.txt with it, reacts if successful or not
     if message.content.startswith('!addname'):
-        name = message.content.split()
         count = 0
-
-        with open(NAMES_FILE, 'r') as names:
-            namelist = names.read().splitlines()
-
-        if len(name) > 10:
-            if is_admin:
-                print("admin added more than 10 names")
-            else:
-                await message.channel.send("Too many names at once! Please limit to under 10")
-                return
-
-        for x in name[1:]:
-            if x not in namelist or len(x) < 20:
-                with open(NAMES_FILE, 'a') as names:
-                    names.write('\n' + x.title())
-                    count += 1
+        for x in message.content.split()[1:]:
+            if names.check_if_valid(x):
+                count += 1
 
         if count >= 1:
-            await message.channel.send("Names added!")
-            return
+            names.add_name(message.content)
+            await message.add_reaction('\N{THUMBS UP SIGN}')
         else:
-            await message.channel.send("Name already in names list!")
+            await message.add_reaction('\N{THUMBS DOWN SIGN}')
             return
 
     # !name
-    """
-    Generates a random two word name from names.txt
-    """
+    # Generates a random two word name from names.txt
     if message.content.startswith('!name'):
-        with open(NAMES_FILE, 'r') as names:
-            names_list = names.read().splitlines()
-            line_count = len(names_list)
-
-        firstname = random.randint(0, line_count - 1)
-        lastname = random.randint(0, line_count - 1)
-
-        await message.channel.send(f"{names_list[firstname]} {names_list[lastname]}")
+        await message.channel.send(names.create_name())
 
     # !help
-    """
-    Returns how to use !addname and !name
-    """
+    # Returns how to use !addname and !name
     if message.content.startswith('!help'):
         await message.channel.send(
             """Use !name to generate a name \nUse !addname to add a name to the list of names to be generated from""")
-
 
 if __name__ == "__main__":
     client.run(TOKEN)
---
+++ name.py
@@ -0,0 +1,66 @@
+import random
+
+
+class Names:
+    """
+    This class pertains to the algorithms and methods related to creating a name from a provided list
+    """
+
+    def __init__(self, file: str):
+        self.file_name = file
+
+    def check_if_valid(self, string):
+        """
+        Takes name string as input, checks if name is valid (below max length, not in names.txt)
+        returns boolean if valid or not
+        """
+        limit = 20
+
+        with open(self.file_name, 'r') as names:
+            namelist = names.read().splitlines()
+
+        if string.title() in namelist or len(string) > limit:
+            return False
+
+        return True
+
+    def add_name(self, string: str):
+
+        """
+        Takes string as input, splits it at whitespace, converts to Title, checks if names in list, appends to names.txt
+        returns None
+        """
+        limit = 20          # allowed max length of a name added
+        name = string.split()
+
+        with open(self.file_name, 'r') as names:
+            namelist = names.read().splitlines()
+
+        if name[0] == '!addname':       # this shouldn't go here but i'm lazy
+            del name[0]
+
+        for x in name:
+            if self.check_if_valid(x):
+                with open(self.file_name, 'a') as names:
+                    names.write('\n' + x.title())
+
+    def create_name(self):
+        """
+        Generates a random name from names list
+        returns string of name
+        """
+
+        with open(self.file_name, 'r') as names:
+            names_list = names.read().splitlines()
+            line_count = len(names_list)
+
+        firstname = random.randint(0, line_count - 1)
+        lastname = random.randint(0, line_count - 1)
+
+        return names_list[firstname] + " " + names_list[lastname]
+
+
+if __name__ == '__main__':
+    test = Names('names.txt')
+    test.add_name("!addname Aborshy Scott")
+    print(test.create_name())
